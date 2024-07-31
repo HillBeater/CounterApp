80b1358aa9e5d939cb9b37bc5df2006694690d3b
Anurag <83819572+HillBeater@users.noreply.github.com>
2024-07-31 00:12:12 -0700

    Code cleanUp

--- app/src/main/java/com/example/counterapp/CounterViewModel.kt
+++ app/src/main/java/com/example/counterapp/CounterViewModel.kt
@@ -4,17 +4,20 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 
-class CounterViewModel : ViewModel() {
-    private val _count = mutableStateOf(0)
+class CounterViewModel() : ViewModel() {
+    private val _repository: CounterRepository = CounterRepository()
+    private val _count = mutableStateOf(_repository.getCounter().count)
 
     // Expose the count as an immutable state
     val count: MutableState<Int> = _count
 
     fun Increment(){
-        _count.value++
+        _repository.IncrementCounter()
+        _count.value = _repository.getCounter().count
     }
 
     fun Decrement(){
-        _count.value--
+        _repository.DecrementCounter()
+        _count.value = _repository.getCounter().count
     }
 }
